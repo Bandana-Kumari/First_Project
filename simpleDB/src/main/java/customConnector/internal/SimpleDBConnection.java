6389cab77db99aa77bd4d8999cff5e9115e2c07d
rajeswari-10426 <rajeswari-10426@users.noreply.github.com>
2019-08-30 16:16:16 +0530

    connector
    custom connector
    
    Co-Authored-By: bandana-kumari <bandana-kumari@users.noreply.github.com>

---
+++ simpleDB/src/main/java/customConnector/internal/SimpleDBConnection.java
@@ -0,0 +1,22 @@
+package customConnector.internal;
+
+
+/**
+ * This class represents an extension connection just as example (there is no real connection with anything here c:).
+ */
+public final class SimpleDBConnection {
+
+  private final String id;
+
+  public SimpleDBConnection(String id) {
+    this.id = id;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void invalidate() {
+    // do something to invalidate this connection!
+  }
+}
